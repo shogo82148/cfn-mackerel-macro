742b26ad89009c587df0546c82928ca25dd80c43
Ichinose Shogo <shogo82148@gmail.com>
2019-04-16 16:46:33 +0900

    add test for creating host metric monitor

--- cfn/monitor_test.go
+++ cfn/monitor_test.go
@@ -66,3 +66,74 @@ func TestCreateMonitor_MonitorConnectivity(t *testing.T) {
 		t.Errorf("unexpected type, want %s, got %s", "connectivity", param["Type"].(string))
 	}
 }
+
+func TestCreateMonitor_MonitorHostMetric(t *testing.T) {
+	ptrFloat64 := func(v float64) *float64 { return &v }
+	m := &monitor{
+		Function: &Function{
+			org: &mackerel.Org{
+				Name: "test-org",
+			},
+			client: &fakeMackerelClient{
+				createMonitor: func(ctx context.Context, param mackerel.Monitor) (mackerel.Monitor, error) {
+					want := &mackerel.MonitorHostMetric{
+						Name:                 "disk.aa-00.writes.delta",
+						Memo:                 "This monitor is for Hatena Blog.",
+						Duration:             3,
+						Metric:               "disk.aa-00.writes.delta",
+						Operator:             ">",
+						Warning:              ptrFloat64(20000.0),
+						Critical:             ptrFloat64(400000.0),
+						MaxCheckAttempts:     3,
+						Scopes:               []string{"Hatena-Blog"},
+						ExcludeScopes:        []string{"Hatena-Bookmark:db-master"},
+						NotificationInterval: 60,
+					}
+					if diff := cmp.Diff(param, want); diff != "" {
+						t.Errorf("monitor differs: (-got +want)\n%s", diff)
+					}
+					want.ID = "3yAYEDLXKL5"
+					return want, nil
+				},
+			},
+		},
+		Event: cfn.Event{
+			RequestType:       cfn.RequestCreate,
+			RequestID:         "",
+			ResponseURL:       "https://cloudformation-custom-resource-response-apnortheast1.s3-ap-northeast-1.amazonaws.com/xxxxx",
+			ResourceType:      "Custom:Monitor",
+			LogicalResourceID: "Monitor",
+			StackID:           "arn:aws:cloudformation:ap-northeast-1:1234567890:stack/foobar/12345678-1234-1234-1234-123456789abc",
+			ResourceProperties: map[string]interface{}{
+				"Type":                 "host",
+				"Name":                 "disk.aa-00.writes.delta",
+				"Memo":                 "This monitor is for Hatena Blog.",
+				"Duration":             3,
+				"Metric":               "disk.aa-00.writes.delta",
+				"Operator":             ">",
+				"Warning":              20000.0,
+				"Critical":             400000.0,
+				"MaxCheckAttempts":     3,
+				"Scopes":               []interface{}{"mkr:test-org:service:Hatena-Blog"},
+				"ExcludeScopes":        []interface{}{"mkr:test-org:role:Hatena-Bookmark:db-master"},
+				"NotificationInterval": 60,
+			},
+		},
+	}
+	id, param, err := m.create(context.Background())
+	if err != nil {
+		t.Error(err)
+	}
+	if id != "mkr:test-org:monitor:3yAYEDLXKL5" {
+		t.Errorf("unexpected host id: want %s, got %s", "mkr:test-org:host:3yAYEDLXKL5", id)
+	}
+	if param["MonitorId"].(string) != "3yAYEDLXKL5" {
+		t.Errorf("unexpected monitor id, want %s, got %s", "3yAYEDLXKL5", param["MonitorId"].(string))
+	}
+	if param["Name"].(string) != "disk.aa-00.writes.delta" {
+		t.Errorf("unexpected name, want %s, got %s", "foo-bar", param["Name"].(string))
+	}
+	if param["Type"].(string) != "host" {
+		t.Errorf("unexpected type, want %s, got %s", "host", param["Type"].(string))
+	}
+}
