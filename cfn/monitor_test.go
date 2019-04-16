0b81105a4936f2fbc3070457c6b784cd55066fc2
Ichinose Shogo <shogo82148@gmail.com>
2019-04-16 16:55:33 +0900

    add test for creating service metric monitor

--- cfn/monitor_test.go
+++ cfn/monitor_test.go
@@ -137,3 +137,77 @@ func TestCreateMonitor_MonitorHostMetric(t *testing.T) {
 		t.Errorf("unexpected type, want %s, got %s", "host", param["Type"].(string))
 	}
 }
+
+func TestCreateMonitor_MonitorServiceMetric(t *testing.T) {
+	ptrFloat64 := func(v float64) *float64 { return &v }
+	ptrUint64 := func(v uint64) *uint64 { return &v }
+	m := &monitor{
+		Function: &Function{
+			org: &mackerel.Org{
+				Name: "test-org",
+			},
+			client: &fakeMackerelClient{
+				createMonitor: func(ctx context.Context, param mackerel.Monitor) (mackerel.Monitor, error) {
+					want := &mackerel.MonitorServiceMetric{
+						Name:                    "Hatena-Blog - access_num.4xx_count",
+						Memo:                    "A monitor that checks the number of 4xx for Hatena Blog",
+						Duration:                1,
+						Service:                 "Hatena-Blog",
+						Metric:                  "access_num.4xx_count",
+						Operator:                ">",
+						Warning:                 ptrFloat64(50.0),
+						Critical:                ptrFloat64(100.0),
+						MaxCheckAttempts:        3,
+						NotificationInterval:    60,
+						MissingDurationWarning:  ptrUint64(360),
+						MissingDurationCritical: ptrUint64(720),
+					}
+					if diff := cmp.Diff(param, want); diff != "" {
+						t.Errorf("monitor differs: (-got +want)\n%s", diff)
+					}
+					want.ID = "3yAYEDLXKL5"
+					return want, nil
+				},
+			},
+		},
+		Event: cfn.Event{
+			RequestType:       cfn.RequestCreate,
+			RequestID:         "",
+			ResponseURL:       "https://cloudformation-custom-resource-response-apnortheast1.s3-ap-northeast-1.amazonaws.com/xxxxx",
+			ResourceType:      "Custom:Monitor",
+			LogicalResourceID: "Monitor",
+			StackID:           "arn:aws:cloudformation:ap-northeast-1:1234567890:stack/foobar/12345678-1234-1234-1234-123456789abc",
+			ResourceProperties: map[string]interface{}{
+				"Type":                    "service",
+				"Name":                    "Hatena-Blog - access_num.4xx_count",
+				"Memo":                    "A monitor that checks the number of 4xx for Hatena Blog",
+				"Service":                 "mkr:test-org:service:Hatena-Blog",
+				"Duration":                1,
+				"Metric":                  "access_num.4xx_count",
+				"Operator":                ">",
+				"Warning":                 50.0,
+				"Critical":                100.0,
+				"MaxCheckAttempts":        3,
+				"MissingDurationWarning":  360,
+				"MissingDurationCritical": 720,
+				"NotificationInterval":    60,
+			},
+		},
+	}
+	id, param, err := m.create(context.Background())
+	if err != nil {
+		t.Error(err)
+	}
+	if id != "mkr:test-org:monitor:3yAYEDLXKL5" {
+		t.Errorf("unexpected host id: want %s, got %s", "mkr:test-org:host:3yAYEDLXKL5", id)
+	}
+	if param["MonitorId"].(string) != "3yAYEDLXKL5" {
+		t.Errorf("unexpected monitor id, want %s, got %s", "3yAYEDLXKL5", param["MonitorId"].(string))
+	}
+	if param["Name"].(string) != "Hatena-Blog - access_num.4xx_count" {
+		t.Errorf("unexpected name, want %s, got %s", "Hatena-Blog - access_num.4xx_count", param["Name"].(string))
+	}
+	if param["Type"].(string) != "service" {
+		t.Errorf("unexpected type, want %s, got %s", "service", param["Type"].(string))
+	}
+}
