d3cd33715eaf6efac0a8207b4f87d27d974d31e8
Ichinose Shogo <shogo82148@gmail.com>
2019-11-12 14:08:23 +0900

    add test for updating monitors

--- cfn/monitor_test.go
+++ cfn/monitor_test.go
@@ -423,3 +423,115 @@ func TestCreateMonitor_MonitorAnomalyDetection(t *testing.T) {
 		t.Errorf("unexpected type, want %s, got %s", "anomalyDetection", param["Type"].(string))
 	}
 }
+
+func TestUpdateMonitor_updateMutable(t *testing.T) {
+	m := &monitor{
+		Function: &Function{
+			org: &mackerel.Org{
+				Name: "test-org",
+			},
+			client: &fakeMackerelClient{
+				updateMonitor: func(ctx context.Context, monitorID string, param mackerel.Monitor) (mackerel.Monitor, error) {
+					want := &mackerel.MonitorConnectivity{
+						Name: "foo",
+					}
+					if diff := cmp.Diff(param, want); diff != "" {
+						t.Errorf("monitor differs: (-got +want)\n%s", diff)
+					}
+					want.ID = "3yAYEDLXKL5"
+					if monitorID != want.ID {
+						t.Errorf("want %s, got %s", want.ID, monitorID)
+					}
+					return want, nil
+				},
+			},
+		},
+		Event: cfn.Event{
+			RequestType:        cfn.RequestUpdate,
+			RequestID:          "",
+			ResponseURL:        "https://cloudformation-custom-resource-response-apnortheast1.s3-ap-northeast-1.amazonaws.com/xxxxx",
+			ResourceType:       "Custom::Monitor",
+			LogicalResourceID:  "Monitor",
+			PhysicalResourceID: "mkr:test-org:monitor:3yAYEDLXKL5",
+			StackID:            "arn:aws:cloudformation:ap-northeast-1:1234567890:stack/foobar/12345678-1234-1234-1234-123456789abc",
+			ResourceProperties: map[string]interface{}{
+				"Type": "connectivity",
+				"Name": "foo",
+			},
+			OldResourceProperties: map[string]interface{}{
+				"Type": "connectivity",
+				"Name": "bar",
+			},
+		},
+	}
+	id, param, err := m.update(context.Background())
+	if err != nil {
+		t.Error(err)
+	}
+	if id != "mkr:test-org:monitor:3yAYEDLXKL5" {
+		t.Errorf("unexpected host id: want %s, got %s", "mkr:test-org:host:3yAYEDLXKL5", id)
+	}
+	if param["MonitorId"].(string) != "3yAYEDLXKL5" {
+		t.Errorf("unexpected monitor id, want %s, got %s", "3yAYEDLXKL5", param["MonitorId"].(string))
+	}
+	if param["Name"].(string) != "foo" {
+		t.Errorf("unexpected name, want %s, got %s", "foo", param["Name"].(string))
+	}
+	if param["Type"].(string) != "connectivity" {
+		t.Errorf("unexpected type, want %s, got %s", "connectivity", param["Type"].(string))
+	}
+}
+
+func TestUpdateMonitor_updateImmutable(t *testing.T) {
+	m := &monitor{
+		Function: &Function{
+			org: &mackerel.Org{
+				Name: "test-org",
+			},
+			client: &fakeMackerelClient{
+				createMonitor: func(ctx context.Context, param mackerel.Monitor) (mackerel.Monitor, error) {
+					want := &mackerel.MonitorConnectivity{
+						Name: "foo-bar",
+					}
+					if diff := cmp.Diff(param, want); diff != "" {
+						t.Errorf("monitor differs: (-got +want)\n%s", diff)
+					}
+					want.ID = "new-id"
+					return want, nil
+				},
+			},
+		},
+		Event: cfn.Event{
+			RequestType:        cfn.RequestUpdate,
+			RequestID:          "",
+			ResponseURL:        "https://cloudformation-custom-resource-response-apnortheast1.s3-ap-northeast-1.amazonaws.com/xxxxx",
+			ResourceType:       "Custom::Monitor",
+			LogicalResourceID:  "Monitor",
+			PhysicalResourceID: "mkr:test-org:monitor:old-id",
+			StackID:            "arn:aws:cloudformation:ap-northeast-1:1234567890:stack/foobar/12345678-1234-1234-1234-123456789abc",
+			ResourceProperties: map[string]interface{}{
+				"Type": "connectivity",
+				"Name": "foo-bar",
+			},
+			OldResourceProperties: map[string]interface{}{
+				"Type": "host",
+			},
+		},
+	}
+	id, param, err := m.update(context.Background())
+	if err != nil {
+		t.Error(err)
+	}
+	if id != "mkr:test-org:monitor:new-id" {
+		t.Errorf("unexpected host id: want %s, got %s", "mkr:test-org:monitor:new-id", id)
+	}
+	if param["MonitorId"].(string) != "new-id" {
+		t.Errorf("unexpected monitor id, want %s, got %s", "new-id", param["MonitorId"].(string))
+	}
+	if param["Name"].(string) != "foo-bar" {
+		t.Errorf("unexpected name, want %s, got %s", "foo-bar", param["Name"].(string))
+	}
+	if param["Type"].(string) != "connectivity" {
+		t.Errorf("unexpected type, want %s, got %s", "connectivity", param["Type"].(string))
+	}
+}
