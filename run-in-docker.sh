03267daa844759d153c4e55c4ec362c797cccd92
Ichinose Shogo <shogo82148@gmail.com>
2021-05-03 17:34:40 +0900

    use docker volume for caching

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -5,6 +5,6 @@ docker volume create cfn-mackerel-macro-cache > /dev/null 2>&1
 docker run --rm -it \
     -e GO111MODULE=on \
     -e GOOS=linux -e GOARCH=amd64 -e CGO_ENABLED=0 \
-    -v "$CURRENT/.mod":/go/pkg/mod \
-    -v "$CURRENT":/go/src/github.com/shogo82148/cfn-mackerel-macro \
+    -v cfn-mackerel-macro-cache:/go/pkg/mod \
+    -v cfn-mackerel-macro-cache:/go/src/github.com/shogo82148/cfn-mackerel-macro \
     -w /go/src/github.com/shogo82148/cfn-mackerel-macro golang:1.16.2 "$@"
