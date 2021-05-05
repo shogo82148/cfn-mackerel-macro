035126914406df7a3a1c74f1d011840fd3b7fb15
Ichinose Shogo <shogo82148@gmail.com>
2021-05-05 22:08:12 +0900

    add TestFindAWSIntegration

--- mackerel/aws_integration_test.go
+++ mackerel/aws_integration_test.go
@@ -76,3 +76,64 @@ func TestFindAWSIntegrations(t *testing.T) {
 		t.Errorf("(-want/+got):\n%s", diff)
 	}
 }
+
+func TestFindAWSIntegration(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `{
+	"id": "46vGJ7uUsp3",
+	"name": "shogo82148",
+	"memo": "",
+	"key": null,
+	"roleArn": "arn:aws:iam::123456789012:role/foobar",
+	"externalId": "hogehoge",
+	"region": "ap-northeast-1",
+	"includedTags": "",
+	"excludedTags": "",
+	"services": {
+		"S3": {
+			"enable": false,
+			"role": null,
+			"excludedMetrics": []
+		}
+	}
+}`)
+	}))
+	defer ts.Close()
+
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := &Client{
+		BaseURL:    u,
+		APIKey:     "DUMMY-API-KEY",
+		HTTPClient: ts.Client(),
+	}
+
+	integrations, err := c.FindAWSIntegration(context.Background(), "46vGJ7uUsp3")
+	if err != nil {
+		t.Error(err)
+	}
+	want := &AWSIntegration{
+		ID:           "46vGJ7uUsp3",
+		Name:         "shogo82148",
+		Memo:         "",
+		RoleArn:      pointer.String("arn:aws:iam::123456789012:role/foobar"),
+		ExternalID:   pointer.String("hogehoge"),
+		Region:       "ap-northeast-1",
+		IncludedTags: "",
+		ExcludedTags: "",
+		Services: map[string]*AWSIntegrationService{
+			"S3": {
+				Enable:          false,
+				Role:            "",
+				ExcludedMetrics: []string{},
+			},
+		},
+	}
+	if diff := cmp.Diff(want, integrations); diff != "" {
+		t.Errorf("(-want/+got):\n%s", diff)
+	}
+}
