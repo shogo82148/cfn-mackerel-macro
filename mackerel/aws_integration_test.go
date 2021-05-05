43a88fc62962c7eeef322712f6abad8c2365f0fb
Ichinose Shogo <shogo82148@gmail.com>
2021-05-05 22:27:46 +0900

    add TestUpdateAWSIntegration

--- mackerel/aws_integration_test.go
+++ mackerel/aws_integration_test.go
@@ -231,3 +231,85 @@ func TestCreateAWSIntegration(t *testing.T) {
 		t.Errorf("(-want/+got):\n%s", diff)
 	}
 }
+
+func TestUpdateAWSIntegration(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			t.Errorf("invalid method: want %s, got %s", http.MethodPut, r.Method)
+		}
+		if r.URL.Path != "/api/v0/aws-integrations/46vGJ7uUsp3" {
+			t.Errorf("invalid path: want %s, got %s", "/api/v0/aws-integrations", r.URL.Path)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `{
+	"id": "46vGJ7uUsp3",
+	"name": "shogo82148",
+	"memo": "",
+	"key": null,
+	"roleArn": "arn:aws:iam::123456789012:role/foobar",
+	"externalId": "hogehoge",
+	"region": "ap-northeast-1",
+	"includedTags": "",
+	"excludedTags": "",
+	"services": {
+		"S3": {
+			"enable": false,
+			"role": null,
+			"excludedMetrics": []
+		}
+	}
+}`)
+	}))
+	defer ts.Close()
+
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := &Client{
+		BaseURL:    u,
+		APIKey:     "DUMMY-API-KEY",
+		HTTPClient: ts.Client(),
+	}
+
+	integrations, err := c.UpdateAWSIntegration(context.Background(), "46vGJ7uUsp3", &AWSIntegration{
+		Name:         "shogo82148",
+		Memo:         "",
+		RoleArn:      pointer.String("arn:aws:iam::123456789012:role/foobar"),
+		ExternalID:   pointer.String("hogehoge"),
+		Region:       "ap-northeast-1",
+		IncludedTags: "",
+		ExcludedTags: "",
+		Services: map[string]*AWSIntegrationService{
+			"S3": {
+				Enable:          false,
+				Role:            "",
+				ExcludedMetrics: []string{},
+			},
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	want := &AWSIntegration{
+		ID:           "46vGJ7uUsp3",
+		Name:         "shogo82148",
+		Memo:         "",
+		RoleArn:      pointer.String("arn:aws:iam::123456789012:role/foobar"),
+		ExternalID:   pointer.String("hogehoge"),
+		Region:       "ap-northeast-1",
+		IncludedTags: "",
+		ExcludedTags: "",
+		Services: map[string]*AWSIntegrationService{
+			"S3": {
+				Enable:          false,
+				Role:            "",
+				ExcludedMetrics: []string{},
+			},
+		},
+	}
+	if diff := cmp.Diff(want, integrations); diff != "" {
+		t.Errorf("(-want/+got):\n%s", diff)
+	}
+}
